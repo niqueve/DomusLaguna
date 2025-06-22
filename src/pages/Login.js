2eb3ea96fb97bd6d556fd983a2fc3fceaaf72b0a
niqueve <niquevellin@gmail.com>
2025-06-22 07:52:54 -0300

    salvar email na localStorege

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 import Header from "../components/HeaderMain";
 import Footer from "../components/Footer";
@@ -17,12 +17,34 @@ const Conteiner = styled.div`
     box-shadow: 0px 4px 4px #00000040;
 `;
 
+const Checkdiv = styled.div`
+    display: flex;
+    align-items: 'center';
+    padding: 1rem;
+    color: #00000050;
+`;
+
 const Login = ()=>{
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+    const [lembrarEmail, setLembrarEmail] = useState(false);
 
-    const handleLogin = () => {
-        console.log('Login', {email, senha});
+    useEffect(() => {
+        const savedEmail = localStorage.getItem('userEmail');
+        if (savedEmail){
+            setEmail(savedEmail);
+            setLembrarEmail(true);
+        }
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(lembrarEmail) {
+            localStorage.setItem('userEmail', email);
+        }else{
+            localStorage.removeItem('userEmail');
+        }
     };
 
     return (
@@ -30,9 +52,15 @@ const Login = ()=>{
             <Header />
             <Conteiner>
                 <h2>Login</h2>
-                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-                <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} />
-                <button onClick={handleLogin}>Entrar</button>
+                <form onSubmit={handleSubmit}>
+                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} autoComplete="email" required />
+                <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} autoComplete="current-password" required/>
+                <Checkdiv>
+                    <input  type="checkbox" id="lembrarEmail" checked={lembrarEmail} onChange={(e) => setLembrarEmail(e.target.checked)}/>
+                    <label htmlFor="lembrarEmail">Lembrar meu email</label>
+                </Checkdiv>
+                <button type="submit">Entrar</button>
+                </form>
             </Conteiner>
             <Footer />
         </>
